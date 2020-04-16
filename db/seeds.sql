10907594070b5082ab7de7711f2246abc4de3bb2
vb27 <vampblood87@gmail.com>
2020-04-16 15:36:48 -0700

    using a different app as the baseline for the code. Changed variable names and routes.

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name) VALUES ('Classic');
+INSERT INTO burgers (name) VALUES ('American Cheese');
+INSERT INTO burgers (name, eaten) VALUES ('Chicken', true);
+INSERT INTO burgers (name, eaten) VALUES ('Double Stack', true);
